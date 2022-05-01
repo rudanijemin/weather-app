5daefa47749c487a129df0e0bd7c3c24ce0ca3a2
jemin <rudanijemin18@gmail.com>
2022-05-01 10:11:48 +0530

    static path

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 8000
 
+
+//public static path
+const static_path = path.join(__dirname,"../public");
+
+app.use(express.static(static_path));
+
+//routing
 app.get("",(req,res)=>{
     res.send("welcome to jemin project");
 })
