b4db25f5ef4984fe796338217a028d7618464c99
jemin <rudanijemin18@gmail.com>
2022-05-03 13:34:41 +0530

    create templates and partiaks

--- src/app.js
+++ src/app.js
@@ -1,29 +1,40 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("hbs");   /*partial use */
 const app = express();
 const port = 8000
 
 
 //public static path
 const static_path = path.join(__dirname,"../public");
+const template_path = path.join(__dirname,"../templates/views");
+const partials_path = path.join(__dirname,"../templates/partials");
+
+app.set('view engine', 'hbs');
+app.set('views', template_path);
+hbs.registerPartials(partials_path);
+
 
 app.use(express.static(static_path));
 
+
+
+
 //routing
 app.get("",(req,res)=>{
-    res.send("welcome to jemin project");
+    res.render('index');
 })
 
 app.get("/about",(req,res)=>{
-    res.send("welcome to jemin about project");
+    res.render('about');
 })
 
 app.get("/weather",(req,res)=>{
-    res.send("welcome to jemin  weather project");
+    res.send("welcome weather APP");
 })
 
 app.get("*",(req,res)=>{
-    res.send("404 error page");
+    res.render("404error");
 })
 
 app.listen(port,()=>{
