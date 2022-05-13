1c15afb480e22a70fa6f34bdf3fc8bdef6dda3d7
jemin <rudanijemin18@gmail.com>
2022-05-13 22:01:09 +0530

    hide value

--- public/js/main1.js
+++ public/js/main1.js
@@ -1,29 +1,30 @@
 const submitBtn = document.getElementById("submitBtn");
 const cityName = document.getElementById("cityName");
 const city_name = document.getElementById("city_name");
-const temp = document.getElementById("temp");
+const temp_real_val = document.getElementById("temp_real_val");
 const temp_status = document.getElementById("temp_status");
+
+const dataHide = document.querySelector(".middle_layer");
 
 
 const getInfo = async(event)=>{
     event.preventDefault();
-  
     let cityVal = cityName.value;
-
         //  let url=https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
     try{
         if(cityVal === "")
         {
             city_name.innerText = "plz write the name before the search";
+            dataHide.classList.add("data_hide");  //koi data no male tyare previous data thai jase 
         }
         else{
-            let url ="https://api.openweathermap.org/data/2.5/weather?q=pune&units=metric&appid=481bc2d6e02d6095205f266f5a8240f9"
+            let url ="https://api.openweathermap.org/data/2.5/weather?q=surat&units=metric&appid=481bc2d6e02d6095205f266f5a8240f9"
             const response = await fetch(url);
             const data = await response.json();
             const arrData = [data];  //data array ma ave etle tene array ma store karava pade
 
-            city_name.innerText = `${arrData[0].name},${arrData[0].sys.country}`;
-            temp.innerText = arrData[0].main.temp;  // url google ma past kari ne joyu
+            city_name.innerText = `${arrData[0].name},${arrData[0].sys.country}`; //country name and city name avi jai
+            temp_real_val.innerText = arrData[0].main.temp;  // url google ma past kari ne joyu
             const tempMood = arrData[0].weather[0].main;
 
             //condition to check sunny or cloudy
@@ -48,10 +49,12 @@ const getInfo = async(event)=>{
                 temp_status.innerHTML=
                     "<i class=`fas fa-sun` style=`color: #eccc68;`></i>"
             }
+            dataHide.classList.remove("data_hide");
         }
     }
     catch{
         city_name.innerText = "plz enter the city name properly";
+        dataHide.classList.add("data_hide");
     }
       
 }
